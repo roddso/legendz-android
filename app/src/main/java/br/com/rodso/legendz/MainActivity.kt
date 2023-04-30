9d1a54a55b1fe875b21feb877e2e70c75f780900
Rodrigo Santos de Oliveira <roddso@gmail.com>
2023-04-30 19:03:57 -0300

    Baseline
    Baseline

---
+++ app/src/main/java/br/com/rodso/legendz/MainActivity.kt
@@ -0,0 +1,27 @@
+package br.com.rodso.legendz
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        if(LegendzApplication.auth.currentUser==null){
+
+        }
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+}
